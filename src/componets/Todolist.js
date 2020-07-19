8dd70ef86de4931847a60e7cff2feb08d23aeb48
briantical <lutbrianivan@gmail.com>
2020-07-19 03:06:49 +0300

    Working useEffect

--- src/componets/Todolist.js
+++ src/componets/Todolist.js
@@ -7,6 +7,22 @@ export default function Todolist() {
   const [title, setTitle] = useState('');
   const { todos, dispatch } = useContext(TodoContext);
 
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => {
+        dispatch({
+          type: SET_TODOS,
+          payload: response.data
+        });
+      })
+      .catch((error) => {
+        dispatch({
+          type: SET_ERROR,
+          payload: error
+        });
+      });
+  }, []);
+
   return <div>{JSON.stringify(todos)}</div>;
 }
-
---
+++ tests/utils/index.js
@@ -0,0 +1,6 @@
+const findByTestAttr = (component, attribute) => {
+  const wrapper = component.find(`[data-test='${attribute}']`);
+  return wrapper;
+};
+
+export default findByTestAttr;
